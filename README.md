793ca7f75d98ff96697b9cd79c671d4e1a76f724
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-11-24 03:48:51 +0100

    feat: initialize project

---
+++ README.md
@@ -0,0 +1,68 @@
+<p align="center">
+  <img alt="Rust ChatGPT Assistant Logo" height="128" src="logo.webp" />
+  <h1 align="center">Rust 2021 ChatGPT Assistant</h1>
+
+</p>
+<p align="center">
+  <a href="https://chatgpt.com/g/g-GjZIE8Rr8-rust-tauri-assistant">
+    <img src="https://img.shields.io/badge/Custom%20ChatGPT-Start%20a%20%20Chat-baa5c3?style=for-the-badge">
+  </a>
+</p>
+
+Rust 2021 Knowledge Base for OpenAI Custom GPTs.
+
+## Documentation Repositories
+
+### Rust Programming Language Book
+
+This directory contains the Markdown files of "The Rust Programming Language Book".
+
+- Directory: `./book`
+- [README](./book/README.md)
+- [Repository](https://github.com/rust-lang/book)
+
+### Rust Language Reference
+
+This directory contains the Markdown files of the primary reference for the Rust programming language.
+
+- Directory: `./reference`
+- [README](./reference/README.md)
+- [Repository](https://github.com/rust-lang/reference)
+
+### Rust By Example
+
+This directory contains the Markdown files to learn Rust with examples.
+
+- Directory: `./rust-by-example`
+- [README](./rust-by-example/README.md)
+- [Repository](https://github.com/rust-lang/rust-by-example)
+
+### Rust Performance Book
+
+This directory contains the Markdown files of "The Rust Performance Book".
+
+- Directory: `./perf-book`
+- [README](./perf-book/README.md)
+- [Repository](https://github.com/nnethercote/perf-book)
+
+### The Dark Arts of Advanced and Unsafe Rust Programming
+
+This directory contains the Markdown files for unsafe Rust programming.
+
+- Directory: `./nomicon`
+- [README](./nomicon/README.md)
+- [Repository](https://github.com/rust-lang/nomicon)
+
+### Rust Edition Guide
+
+This directory contains the Markdown files explaining the concept of "editions", major new eras in Rust's development.
+
+- Directory: `./edition-guide`
+- [README](./edition-guide/README.md)
+- [Repository](https://github.com/rust-lang/edition-guide)
+
+## Contribute
+
+```
+make update
+```
---
+++ update.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+set -e
+
+echo "================================================================================"
+echo "[SUBMODULES] Updating submodules..."
+git submodule update --init
+echo "[SUBMODULES] Updating submodules... Done."
+
+# echo "================================================================================"
+# echo "[CRATES.IO] Updating database dumps..."
+# rm -f ./db-dump.tar.gz
+# wget https://static.crates.io/db-dump.tar.gz
+# tar -xf ./db-dump.tar.gz
+# rm -f ./db-dump.tar.gz
+# echo "[CRATES.IO] Updating database dumps... Done."
+
+echo "================================================================================"
+echo "[GENERATION] Generating Custom GPT documentation..."
+find ./.output -mindepth 1 ! -name 'README.md' -exec rm -Rf {} +
+sleep 1 # Wait for the filesystem to catch up
+declare -a documentation_subdirectories=("book" "edition-guide" "nomicon" "perf-book" "reference" "rust-by-example")
+for subdir in "${documentation_subdirectories[@]}"; do
+  search_path="./$subdir/src"
+
+  find "${search_path}" -type f -name "*.md" | while read -r src_file; do
+    dest_file="${src_file/.\//./.output/}" # Replace './' with './.output/' at the beginning
+    dest_file="${dest_file/src\//}" # Remove '/src/' part
+
+    mkdir -p "$(dirname "$dest_file")"
+    cp "$src_file" "$dest_file"
+  done
+done
+echo "[GENERATION] Generating Custom GPT documentation... Done."
+
+echo "================================================================================"
+echo "[INDEXING] Updating Custom GPT documentation REAME..."
+cat ./.output/README.md | sed '/## File List/q' >> ./.output/README.md.temp
+mv ./.output/README.md.temp ./.output/README.md
+sleep 1 # Wait for the filesystem to catch up
+{
+    echo ""
+    find ./.output -type f -name "*.md" | grep -v 'README.md' | while read -r src_file; do
+        # Get the relative file path
+        relative_path="${src_file/.\//}"
+        relative_path="${relative_path/.output\//}"
+
+        # Format as markdown link
+        echo "- [${relative_path}](${relative_path})"
+    done
+    echo ""
+} >> ./.output/README.md
+echo "[INDEXING] Updating Custom GPT documentation README... Done."
+
+echo "================================================================================"
+echo "[ARCHIVING] Generating Custom GPT Knowledge Base..."
+rm -Rf ./*.tar.gz
+version_tag=$(date "+%Y.%m.%d")
+archive_name="custom_gpt_rust-v2021_knowledge_base-v${version_tag}.tar.gz"
+pushd ./.output
+tar -czf "${archive_name}" *
+popd
+mv "./.output/${archive_name}" "./${archive_name}"
+echo "[ARCHIVING] Generating Custom GPT Knowledge Base... Done."
+
+echo "================================================================================"
+echo ""
+echo "You can now upload this archive to your Custom GPT Assistant: ./${archive_name}."
